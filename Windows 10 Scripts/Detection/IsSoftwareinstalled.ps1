a397e276ed8cbf4a0ae618bd30ca2064f0946bfc
Donovan Sobrero <71948474+SobreroD@users.noreply.github.com>
2020-10-30 20:19:06 -0700

    Update IsSoftwareinstalled.ps1

--- Windows 10 Scripts/Detection/IsSoftwareinstalled.ps1
+++ Windows 10 Scripts/Detection/IsSoftwareinstalled.ps1
@@ -1,6 +1,13 @@
 <#
 By Donovan Sobrero
 Date: 10/30/202
+
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Format-Table –AutoSize
+
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Format-Table –AutoSize
+
 check if software is installed
 $software = what is shown in add remove programs
 #>
